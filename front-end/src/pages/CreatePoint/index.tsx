036ec9c0b5b3f80a88359614257461352dda0b7d
Lusca05 <lucasaugustocastro24@gmail.com>
2020-06-08 15:21:49 -0300

    voltando as configuraçoes normais

--- front-end/src/pages/CreatePoint/index.tsx
+++ front-end/src/pages/CreatePoint/index.tsx
@@ -124,37 +124,43 @@ const CreatePoint = () => {
   const handleSubmit = useCallback((event: FormEvent) => {
     event.preventDefault();
 
-    setIsModalVisible(true);
+    const { name, email, whatsapp } = formData;
+    const uf = selectedUf;
+    const city = selectedCity;
+    const [latitude, longitude] = selectedPosition;
+    const items = selectedItems;
 
-    // const { name, email, whatsapp } = formData;
-    // const uf = selectedUf;
-    // const city = selectedCity;
-    // const [latitude, longitude] = selectedPosition;
-    // const items = selectedItems;
+    const data = new FormData();
 
-    // const data = new FormData();
+    data.append('name', name);
+    data.append('email', email);
+    data.append('whatsapp', whatsapp);
+    data.append('uf', uf);
+    data.append('city', city);
+    data.append('latitude', String(latitude));
+    data.append('longitude', String(longitude));
+    data.append('items', items.join(','));
 
-    // data.append('name', name);
-    // data.append('email', email);
-    // data.append('whatsapp', whatsapp);
-    // data.append('uf', uf);
-    // data.append('city', city);
-    // data.append('latitude', String(latitude));
-    // data.append('longitude', String(longitude));
-    // data.append('items', items.join(','));
+    if(selectedFile) {
+      data.append('image', selectedFile);
 
-    // if(selectedFile) {
-    //   data.append('image', selectedFile);
+    }
 
-    // }
-
-    // api.post('points', data);
+    api.post('points', data);
    
-    
-    // setTimeout(()=> history.push('/'), 2000);
+    setIsModalVisible(true);
+    setTimeout(()=> history.push('/'), 2000);
 
   },
-  [setIsModalVisible]);
+  [
+    formData, 
+    selectedCity,
+    selectedFile,
+    selectedItems,
+    selectedPosition,
+    selectedUf,
+    history
+  ]);
 
   
 
